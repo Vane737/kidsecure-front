60f35f9173a8ee22620785a0586212b30fd78a0a
Vane737 <77525733+Vane737@users.noreply.github.com>
2023-10-03 11:42:35 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import './App.css'
+// import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='bg-red-700'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
