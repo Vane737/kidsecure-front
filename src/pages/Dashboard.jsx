72bb106a1eaf37364e6904c337da17b3eaa95d2c
Vane737 <77525733+Vane737@users.noreply.github.com>
2023-12-14 20:18:10 -0400

    add card functional

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,57 +1,40 @@
+import React from 'react';
 import { UserIcon, BellAlertIcon, ArrowRightCircleIcon, IdentificationIcon } from '@heroicons/react/24/solid';
+import { useNavigate } from 'react-router-dom';
 
-export const  Dashboard = () => {
+const Card = ({ title, subtitle, icon, color, route }) => {
+  const navigation = useNavigate();
 
-    return(
-        <div className="w-full p-10 container h-60 grid grid-cols-3 gap-6">
-            <div className="bg-customGreen rounded-md shadow-md text-white">
-                <div className="flex justify-between items-center m-10">
-                    <div>
-                        <p className="text-lg font-semibold text-gray-700">Administrar niños</p>
-                        <p className="text-2xl font-bold text-gray-800">57 inscritos</p>
-                    </div>
-                    <div className="bg-emerald-300 text-emerald-700 p-3 rounded-full mr-4">
-                    <UserIcon  className="w-10 h-10"/>    
-                    </div>
-                </div>
-                <div className='bg-emerald-500 w-full h-10 text-center rounded-b-md flex justify-center items-center '>
-                    <ArrowRightCircleIcon className='w-6 h-6' /> 
-                    &nbsp;
-                    Ver mas..
-                </div>        
-            </div>
-            <div className="bg-customPink rounded-md shadow-md text-white">
-                <div className="flex justify-between items-center m-10">
-                    <div>
-                        <p className="text-lg font-semibold text-gray-700">Identificacion de violencia</p>
-                        <p className="text-2xl font-bold text-gray-800">57 inscritos</p>
-                    </div>
-                    <div className="bg-pink-300 text-pink-700 p-3 rounded-full mr-4">
-                    <BellAlertIcon  className="w-10 h-10"/>    
-                    </div>
-                </div>
-                <div className='bg-pink-500 w-full h-10 text-center rounded-b-md flex justify-center items-center '>
-                    <ArrowRightCircleIcon className='w-6 h-6' /> 
-                    &nbsp;
-                    Ver mas..
-                </div>        
-            </div>
-            <div className="bg-secondary rounded-md shadow-md text-white">
-                <div className="flex justify-between items-center m-10">
-                    <div>
-                        <p className="text-lg font-semibold text-gray-700">Administra tus contactos</p>
-                        <p className="text-2xl font-bold text-gray-800">57 inscritos</p>
-                    </div>
-                    <div className="bg-yellow-200 text-yellow-600 p-3 rounded-full mr-4">
-                    <IdentificationIcon  className="w-10 h-10"/>    
-                    </div>
-                </div>
-                <div className='bg-yellow-400 w-full h-10 text-center rounded-b-md flex justify-center items-center '>
-                    <ArrowRightCircleIcon className='w-6 h-6' /> 
-                    &nbsp;
-                    Ver mas..
-                </div>        
-            </div>  
+  const handleCardClick = () => {
+    navigation(route); // Redirige a la ruta proporcionada
+  };
+
+  return (
+    <div className={`bg-${color} rounded-md mb-10 shadow-md text-white cursor-pointer`} onClick={handleCardClick}>
+      <div className="flex justify-between items-center m-10">
+        <div>
+          <p className="text-lg font-semibold text-gray-700">{title}</p>
+          <p className="text-2xl font-bold text-gray-800">{subtitle}</p>
         </div>
-    ); 
-}
+        <div className={`bg-${color}-300 mt-5 text-${color}-700 px-3 rounded-full mr-4`}>
+          {icon}
+        </div>
+      </div>
+      <div className={`bg-${color}-500 w-full h-10 text-center rounded-b-md flex justify-center items-center`}>
+        <ArrowRightCircleIcon className='w-6 h-6' />
+        &nbsp;
+        Ver más..
+      </div>
+    </div>
+  );
+};
+
+export const Dashboard = () => {
+  return (
+    <div className="w-full p-10 container h-60 grid grid-cols-3 gap-6">
+      <Card title="Administrar niños" subtitle="57 inscritos" icon={<UserIcon className="w-10 h-10" />} color="customGreen" route="/niños" />
+      <Card title="Identificación de actos de violencia" subtitle="Verificar contenido" icon={<BellAlertIcon className="w-10 h-10" />} color="customPink" route="/deteccion/realtime" />
+      <Card title="Administra tus contactos" subtitle="" icon={<IdentificationIcon className="w-10 h-10" />} color="secondary" route="/contacto" />
+    </div>
+  );
+};
